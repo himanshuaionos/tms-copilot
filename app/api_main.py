ddf0312343b258ecfb2bf525474ebbee4dd4d769
Divyesh Chhabra <divyesh.chhabra@aionos.ai>
2025-09-03 23:06:01 +0530

    Updating the API port number

--- app/api_main.py
+++ app/api_main.py
@@ -514,7 +514,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "api_main:app",
         host="0.0.0.0",
-        port=8000,
+        port=8503,
         reload=True,
         log_level="info"
     )
